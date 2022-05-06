fcc81b3b26216a78cd4e50fcef7af3778d8d0624
Antisa184 <antisa184@gmail.com>
2022-05-06 18:23:14 +0200

    Added tests for Inventory

--- spec/inventory_spec.rb
+++ spec/inventory_spec.rb
@@ -1,17 +1,53 @@
 require 'rspec'
+require 'spec_helper'
 
 describe 'Inventory' do
   before do
-    # Do nothing
+    @inventory=Inventory::Base.new
+    @item=Item::Base.new
   end
 
-  after do
-    # Do nothing
+  context 'when gain_gold' do
+    before(:example) do
+      @inventory.gain_gold(10)
+    end
+    it 'increases gold by 10' do
+      expect(@inventory.gold).to eq(110)
+    end
   end
 
-  context 'when condition' do
-    it 'succeeds' do
-      pending 'Not implemented'
+  context 'when lose_gold' do
+    before(:example) do
+      @inventory.lose_gold(10)
+    end
+    it 'decreases gold by 10' do
+      expect(@inventory.gold).to eq(90)
+    end
+  end
+
+  context 'when item_add' do
+    before(:example) do
+      @inventory.item_add(@item)
+    end
+    it 'has 1 item in slots' do
+      expect(@inventory.slots.size).to eq(1)
+    end
+    context 'when item_remove' do
+      before(:example) do
+        @inventory.item_remove(@item)
+      end
+      it 'has 0 item in slots' do
+        expect(@inventory.slots.size).to eq(0)
+      end
+    end
+  end
+
+  context 'when upgrade_slots' do
+    before(:example) do
+      @inventory.upgrade_slots(20)
+    end
+    it 'decreases gold by 10' do
+      expect(@inventory.max_slots).to eq(20)
     end
   end
 end
