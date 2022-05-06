779f14718c192e5a85d3b21464ebc4d8ed259686
Antisa184 <antisa184@gmail.com>
2022-05-06 18:51:52 +0200

    Added tests for NPC

--- spec/npc_spec.rb
+++ spec/npc_spec.rb
@@ -1,17 +1,27 @@
 require 'rspec'
-
+require 'spec_helper'
 describe 'NPC' do
   before do
-    # Do nothing
+    @enemy = NPC::Enemy.new
+    @enemyBoss = NPC::EnemyBoss.new
+    @shop = NPC::Shop.new
+    @questgiver = NPC::QuestGiver.new
+    @player = Player::Base.new
+    @quest = Quest::Base.new
   end
 
-  after do
-    # Do nothing
-  end
-
-  context 'when condition' do
-    it 'succeeds' do
-      pending 'Not implemented'
+  context 'when enemy deals damage to player' do
+    it 'deals 8 damage' do
+      expect(@enemy.deal_damage(@player, false)).to eq(8)
     end
   end
+
+  context 'when enemy takes damage' do
+    it 'take 3 damage' do
+      expect(@enemy.take_damage(5)).to eq(3)
+    end
+  end
+  #ENEMYBOSS SAME AS ENEMY
+  #SHOP AND QUESTGIVER NOTHING TO TEST
+
 end
